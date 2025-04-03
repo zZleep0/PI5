e56d3c7869871a692843d01c4ad43ee343cd0f3a
zZleep0 <152449401+zZleep0@users.noreply.github.com>
2025-04-03 18:10:22 -0300

    fase 1 essenciais

--- Projeto Integrador 5/Assets/Scripts/Fase 1/GarrafaControler.cs
+++ Projeto Integrador 5/Assets/Scripts/Fase 1/GarrafaControler.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class GarrafaControler : MonoBehaviour
 {
     public float qtdAgua = 20f;
+    public ParticleSystem aguaParticula;
 
     public float rotacaoZ;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        aguaParticula = GetComponentInChildren<ParticleSystem>();
+        aguaParticula.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,15 +20,18 @@ public class GarrafaControler : MonoBehaviour
         rotacaoZ = transform.eulerAngles.z;
 
         // Usa transform.eulerAngles para acessar a rotação de Euler
-        if (rotacaoZ >= 135 && rotacaoZ <= 225) // Ajuste do intervalo para evitar inconsistências de rotação
+        if (rotacaoZ >= 135 && rotacaoZ <= 225/* && qtdAgua > 0*/) // Ajuste do intervalo para evitar inconsistências de rotação
         {
             qtdAgua = qtdAgua - Time.deltaTime * 2;
+            aguaParticula.gameObject.SetActive(true);
             Debug.Log("Está derramando água");
+
 
 
         }
         else
         {
+            aguaParticula.gameObject.SetActive(false);
             Debug.Log("Não está derramando água");
         }
     }
